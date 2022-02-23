83640629fd66b030b1bd3de4f421b11de0997a79
sabrinadevasconcelosfernandes <sabrinadvf@ibm.com>
2022-02-23 14:44:57 -0300

    Initial commit

---
+++ app/src/main/java/com/sabsrocambole/swoosh2/activity_league.kt
@@ -0,0 +1,11 @@
+package com.sabsrocambole.swoosh2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_league : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
